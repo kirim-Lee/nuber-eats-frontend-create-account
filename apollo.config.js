e78df4002b27db1a9b4694ec3c7d75ddbc736926
Kirim-Lee <uthi1004@gmail.com>
2021-01-28 10:32:11 +0900

    경로 수정 및 gitignore 추가

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   client: {
-    includes: ["./src/**/*.{tsx,ts}"],
-    tagname: "gql",
+    includes: ['./src/**/*.{tsx,ts}'],
+    tagname: 'gql',
     service: {
-      name: "podcast-backend",
-      url: "https://nuber-eats-challenge.herokuapp.com/graphql"
-    }
-  }
+      name: 'podcast-backend',
+      url: 'https://nuber-eats-assignment.herokuapp.com/graphql',
+    },
+  },
 };
